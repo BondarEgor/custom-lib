8471487785f6e6df716b5d7e62110ad4113d8427
BondarEgor <erop961515@gmail.com>
2024-06-26 00:56:32 +1000

    Добавил первые наброски кнопки

---
+++ my-ui-lib/src/lib/button/button.directive.ts
@@ -0,0 +1,46 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Directive,
+  ElementRef,
+  HostListener,
+  Input,
+  OnInit,
+  Renderer2,
+  ViewEncapsulation,
+} from '@angular/core';
+
+@Component({
+  standalone: true,
+  template: '',
+  styleUrls: ['./button.css'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MyButtonStyle {}
+
+@Directive({
+  standalone: true,
+  selector: '[eyButton]',
+  host: {
+    class: 'ey-button',
+  },
+})
+export class MyButtonDirective implements OnInit {
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+  @Input() size: 's' | 'm' | 'l' | 'xl' = 'm';
+  @Input() appearance: 'primary' | 'secondary' = 'secondary';
+  @Input() shape: null | 'rounded' | 'square' = null
+  ngOnInit(): void {
+    this.setButtonStyles();
+  }
+
+  setButtonStyles() {
+    this.renderer.addClass(this.el.nativeElement, `ey-button--${this.size}`);
+    this.renderer.addClass(
+      this.el.nativeElement,
+      `ey-button--${this.appearance}`
+    );
+    this.renderer.addClass(this.el.nativeElement, `ey-button-${this.shape}`)
+  }
+}
