e490dbf39afa036ce1ddc36ea4f60f5926a6c04b
BondarEgor <erop961515@gmail.com>
2024-07-06 22:28:20 +1000

    fixes expand

--- ey-ui/core/components/expand/expand.component.ts
+++ ey-ui/core/components/expand/expand.component.ts
@@ -1,30 +1,21 @@
-import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { trigger, state, style, transition, animate } from '@angular/animations';
-
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { expandCollapse } from '../../../cdk/animations'
 @Component({
   templateUrl: './expand.component.html',
   styleUrls: ['./expand.component.less'],
   standalone: true,
   selector: 'ey-expand',
   imports: [CommonModule],
-  animations: [
-    trigger('expandCollapse', [
-      state('expanded', style({
-        opacity: 1,
-        maxHeight: '1000px',
-      })),
-      state('collapsed', style({
-        opacity: 0,
-        maxHeight: '0px',
-      })),
-      transition('expanded <=> collapsed', [
-        animate('600ms ease-in-out')
-      ])
-    ])
-  ]
+  animations: [expandCollapse],
 })
 
-export class ExpandComponent {
+export class ExpandComponent implements OnChanges {
   @Input() expanded = false;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['expanded']) {
+      this.expanded = changes['expanded'].currentValue
+    }
+  }
 }
